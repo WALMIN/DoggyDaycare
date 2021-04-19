ce0099d5d6989d6880800829bf4b5d0c06a1d255
Victor Bergene <v@walmin.com>
2021-04-19 12:48:23 +0200

    Added the start page & the dogs list page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
 import './App.css';
+import { useState } from 'react';
+import Home from './components/Home';
+import Dogs from './components/Dogs';
 
 function App() {
+  const HOME = 'home', DOGS = 'dogs';
+  const [currentScreen, setCurrentScreen] = useState(HOME);
+
+  let content = null;
+  switch(currentScreen) {
+    case HOME:
+      content = <Home />
+
+      break;
+
+    case DOGS:
+      content = <Dogs />
+
+      break;
+
+    default:
+      content = <Home />
+
+  }
+
   return (
     <div className="App">
-      <p>Doogy Daycare</p>
+      <header className="Header">
+        <h2>Doggy Daycare</h2>
+        <nav>
+          <button onClick={ () => setCurrentScreen(HOME) }>Home</button>
+          <button onClick={ () => setCurrentScreen(DOGS) }>Dogs</button>
+        </nav>
+      </header>
+      <main>
+        {content}
+      </main>
     </div>
   );
 }
---
+++ src/components/Dogs.js
@@ -0,0 +1,7 @@
+const Dogs = () => (
+    <div>
+        <h1>Dogs</h1>
+    </div>
+)
+
+export default Dogs;
